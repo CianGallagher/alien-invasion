5763f78533118db1c46b720907cd1a1fe0802203
Gally <70110451+CianGallagher@users.noreply.github.com>
2022-08-20 13:13:25 +0100

    Fixed ship moving right after KEYUP indentation error on check_event functions if statement

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,11 +35,14 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right
                     self.ship.moving_right = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
                     # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
